65a162eb49693374920423aa28e0a1d684daf1f3
Gourab Biswas <gourab1331@outlook.com>
2025-04-02 14:37:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+function App() {
+  return (
+    <div className="App">
+      {/* <Home/>
+      <About/> */}
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
